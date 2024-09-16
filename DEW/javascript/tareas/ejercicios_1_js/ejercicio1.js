0f9d45b9f44a06c48a210bb0fe4c081b5eaac193
GRMiguelAngel <miguelangel2e2005@gmail.com>
2024-09-16 12:03:57 +0200

    Subida de ejercicio1

--- DEW/javascript/tareas/ejercicios_1_js/ejercicio1.js
+++ DEW/javascript/tareas/ejercicios_1_js/ejercicio1.js
@@ -1,24 +1,67 @@
 function three_var_control(){
 
-    let a, b, c;
-    a = parseFloat(document.getElementById("num1_ejer1").value);
-    b = parseFloat(document.getElementById("num2_ejer1").value);
-    c = parseFloat(document.getElementById("num3_ejer1").value);
+    let array = [
+    parseFloat(document.getElementById("num1_ejer1").value),
+    parseFloat(document.getElementById("num2_ejer1").value),
+    parseFloat(document.getElementById("num3_ejer1").value)
+    ]
 
-    if (a < 0 || b < 0 || c < 0){
+    if (lower_than(array,0)){
         message = "Error: hay variables con valores negativos.";
     } 
-    else if (a == 0 && b == 0 && c == 0){
+    else if (equal_to(array,0)){
         message = "Error: todas las variables son iguales a cero.";
     }
-    else if (a + b + c > 10 && a != b && b != c && c != a){
-        console.log(a+b+c)
+    else if (greater_than(array, 10) && diferent_than(array)){
         message = "Error: la suma total de las variables es mayor a 10, siendo todas estas diferentes."
     }
     else {
         message = "Todo esta correcto."
     }
-
-    document.getElementById("result1").textContent = message
+    document.getElementById("result1").textContent = message;
     console.log(message);
+}
+
+function lower_than(all_nums, limit)
+{
+    is_lower = false;
+    for (index = 0; index < all_nums.length; index++){
+        if (all_nums[index] < limit){
+            is_lower = true;
+            break;
+        }
+    }
+
+    return is_lower;
+}
+
+function equal_to(all_nums, comparator_num)
+{
+    is_equal = true;
+    for (index = 0; index < all_nums.length; index++){
+        if (all_nums[index] != comparator_num){
+            is_equal = false;
+            break;
+        }
+    }
+    return is_equal;
+}
+
+function greater_than(all_nums, limit)
+{
+    is_greater = false;
+    num_sum = all_nums[0];
+    for (index = 1; index < all_nums.length; index++){
+        num_sum += all_nums[index];
+        }
+    if (num_sum > limit){
+        is_greater = true;
+    }
+    return is_greater;
+    }
+
+
+function diferent_than(all_nums)
+{
+    return equal_to(all_nums,all_nums[1]) && equal_to(all_nums,all_nums[2]) && equal_to(all_nums,all_nums[3]);
 }
