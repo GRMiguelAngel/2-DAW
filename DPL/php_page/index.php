5e9b6e2ff3b7d9840d9366f3bd67ae6b3971b8e4
GRMiguelAngel <miguelangel2e2005@gmail.com>
2024-09-20 16:10:24 +0100

    Subida de nuevos contenidos (php)

---
+++ DPL/php_page/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get" action="get_post.php">
+        Nombre: <input type="submit" name="enviar" value="enviar">
+    </form>
+</body>
+</html>
