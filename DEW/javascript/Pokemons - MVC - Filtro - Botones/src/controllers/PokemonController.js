b74ba8d650fbd3a987665cbe6fc09bf4dff322f6
GRMiguelAngel <miguelangel2e2005@gmail.com>
2024-10-07 17:48:03 +0100

    Se actualiza la pokeapi

---
+++ DEW/javascript/Pokemons - MVC - Filtro - Botones/src/controllers/PokemonController.js
@@ -0,0 +1,97 @@
+// PokemonController.js
+import { PokemonModel } from "../models/PokemonModel.js";
+import { PokemonView } from "../views/PokemonView.js";
+export class PokemonController {
+  constructor() {
+    this.model = new PokemonModel();
+    this.view = new PokemonView();
+
+    this.pokemonsFiltered = [];
+    this.newDesireList = [];
+
+    // Bind button event
+    document
+      .querySelector("button")
+      .addEventListener("click", () => this.init());
+  }
+  async init() {
+    this.view.showLoading();
+    try {
+      await this.model.loadPokemons();
+      this.view.hideLoading();
+      this.view.showConsole();
+      this.view.displayPokemons(this.model.getAllPokemons());
+      this.bindingEvents();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async bindingEvents() {
+    // Bind input filterWeight
+    this.filterWeight = document.querySelector("#filtroPeso");
+    this.filterWeight.addEventListener("keyup", () => this.pokemonsFiltering());
+
+    // Bind input filterTotalPower
+    this.filterTotalPower = document.querySelector("#filtroPoderTotal");
+    this.filterTotalPower.addEventListener("keyup", () => this.pokemonsFiltering());
+    // Bind input filterType
+    this.filterType = document.querySelector("#filtroTipo");
+    this.filterType.addEventListener("keyup", () => this.pokemonsFiltering());
+
+    // Bind Añadir a Lista de deseos
+    document
+      .querySelector("#btnAgnadeListaDeseo")
+      .addEventListener("click", this.mostrarListaDeseo.bind(this));
+
+    // Bind Cards pokemons
+    this.cardPokemons = document.querySelectorAll(".card");
+    this.cardPokemons.forEach((card) => {
+      card.addEventListener("click", () => this.pokemonsClicked(card.id));
+    });
+  }
+
+  pokemonsClicked(cardId) {
+    this.newDesireList.push(cardId);
+  }
+
+  async pokemonsFiltering() {
+    this.pokemonsFiltered = [];   
+    this.model.pokemons.forEach((pkm) => {
+      this.safePokemon = true;
+
+      if (!pkm.pkm_type[0].type.name.includes(this.filterType.value)) {
+        this.safePokemon = false;
+      } else if (
+        pkm.pkm_type.length <= 1 &&
+        !pkm.pkm_type[1].type.name.includes(this.filterType.value)
+      ) {
+        this.safePokemon = false;
+      } else if (!pkm.weight.toString().includes(this.filterWeight.value)) {
+        this.safePokemon = false;
+      } else if (!pkm.attack.toString().includes(this.filterTotalPower.value)) {
+        this.safePokemon = false;
+      }
+      if (this.safePokemon) {
+        this.pokemonsFiltered.push(pkm);
+      }
+    });
+    this.view.displayPokemons(this.pokemonsFiltered);
+  }
+
+  mostrarListaDeseo() {
+    //console.log(this.newDesireList);
+    let txt = "¿Quieres añadir los siguientes Pokemons a la Lista de Deseo?";
+    this.newDesireList.forEach((pkm) => {
+      txt = txt + " " + pkm;
+    });
+
+    if (window.confirm(txt)) {
+      // ToDo Guardar en BBDD
+      console.log("Guardando nueva lista de deseo...");
+    } else if (window.confirm("¿Quieres deseleccionar los pokemons?")) {
+      // ToDo desmarcar pokemons
+
+      this.newDesireList = [];
+    }
+  }
+}
