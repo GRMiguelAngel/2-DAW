25d2b7433c4399e820a31a97324f66d8c8467a2b
GRMiguelAngel <miguelangel2e2005@gmail.com>
2024-10-09 17:49:51 +0100

    Se añaden imagenes de proyecto_LoL

--- DOR/proyecto_LoL/src/js/champions.js
+++ DOR/proyecto_LoL/src/js/champions.js
@@ -1,8 +1,9 @@
 export default class Champion {
 
 constructor(data) {
-    this.name = data.name;                          // Champion's name
     this.id = data.id;                              // Champion's id
+    this.name = data.name;                          // Champion's name
+    this.title = data.title;
     this.image = "https://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + this.id + "_0.jpg";
     this.role = data.tags[0];
     this.playstyle = data.tags[1];    
--- DOR/proyecto_LoL/src/js/index.js
+++ DOR/proyecto_LoL/src/js/index.js
@@ -10,8 +10,8 @@ async function get_champions() {
     for (const key in data) {
         champions.push(new Champion(data[key]));
     }
+    showChampions();
 }
-
 
 get_champions();
 
@@ -19,22 +19,12 @@ console.log(champions);
 
 const showChampions = async () => {
     const champs = document.getElementById("all_champions");
-    // Iteramos sobre cada elemento del array pokemons
-    for(this.id of champions) {
-        }
-        // Para cada Pokemon, se crea una tarjeta con imágenes (vista frontal y trasera), el nombre y los tipos
-        // Esta estructura HTML se añade dinámicamente al contenedor pokedex
-        pokedex.innerHTML +=    `<div class="card">
-                                    <img class="back" src="${champions[this.id]}">
-                                    <img class="front" src=""><br>
-                                    ${pokemons[index].id}. ${pokemons[index].name}<br>
-                                    <div class="types">
-                                    ${tipo1} ${tipo2}
-                                    </div>
-                                    <div class="size">
-                                    height: ${pokemons[index].pkm_height}
-                                    weight: ${pokemons[index].pkm_weight}
-                                    </div>
+    for(let champion of champions) {
+
+        champs.innerHTML +=    `<div class="front">
+                                    <div class="name">${champion.name}, ${champion.title}</div>
+                                    <img class="image" src="${champion.image}">
                                 </div>`
     }
 }
+
