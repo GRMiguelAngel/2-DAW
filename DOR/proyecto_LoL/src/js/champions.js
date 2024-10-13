c06019e4dfb59904e598f6ad0ebbd5bdf0e7a1a1
GRMiguelAngel <miguelangel2e2005@gmail.com>
2024-10-13 13:24:03 +0200

    proyecto de LoL terminado

--- DOR/proyecto_LoL/src/js/champions.js
+++ DOR/proyecto_LoL/src/js/champions.js
@@ -5,7 +5,8 @@ constructor(data) {
     this.name = data.name;
     this.title = data.title;
     this.blurb = data.blurb;
-    this.image = "https://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + this.id + "_0.jpg";
+    this.image = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${this.id}_0.jpg`;
+    this.sprite = `https://ddragon.leagueoflegends.com/cdn/14.20.1/img/champion/${this.id}.png`;
     this.role = data.tags[0];
     this.playstyle = data.tags[1];
     this.stats = data.stats;
--- DOR/proyecto_LoL/src/js/detail.js
+++ DOR/proyecto_LoL/src/js/detail.js
@@ -7,6 +7,8 @@ export function show_detail() {
             if (detail.style.display === 'none' ||  detail.style.display === '') {
 
                 detail.style.display = 'block';
+                detail.style.width = '80%';
+                detail.style.height = '80%';
             } else {
                 detail.style.display = 'none';
             }
